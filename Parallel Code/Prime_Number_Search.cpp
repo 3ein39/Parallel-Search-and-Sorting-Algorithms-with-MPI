d33586058f6b0f38516d799d1774365ebe1a61e0
Moaz Ayman <moaz.ayman.mahmoud@gmail.com>
2025-05-07 17:03:11 +0300

    Refactor Prime Number Search code for improved readability and consistency

--- Parallel Code/Prime_Number_Search.cpp
+++ Parallel Code/Prime_Number_Search.cpp
@@ -2,21 +2,29 @@
 #include <cstdio>
 #include <mpi.h>
 
-bool isPrime(int n) {
-    if (n <= 1) return false;
+using namespace std;
+
+bool isPrime(int n)
+{
+    if (n <= 1)
+        return false;
     for (int i = 2; i * i <= n; ++i)
-        if (n % i == 0) return false;
+        if (n % i == 0)
+            return false;
     return true;
 }
 
-std::vector<int> findPrimes(int start, int end) {
-    std::vector<int> primes;
+vector<int> findPrimes(int start, int end)
+{
+    vector<int> primes;
     for (int i = start; i <= end; ++i)
-        if (isPrime(i)) primes.push_back(i);
+        if (isPrime(i))
+            primes.push_back(i);
     return primes;
 }
 
-void parallelPrimeSearch(int start, int end) {
+void parallelPrimeSearch(int start, int end)
+{
     int rank, size;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
@@ -27,25 +35,28 @@ void parallelPrimeSearch(int start, int end) {
     int local_start = start + rank * chunk + (rank < remainder ? rank : remainder);
     int local_end = local_start + chunk - 1 + (rank < remainder ? 1 : 0);
 
-    std::vector<int> local_primes = findPrimes(local_start, local_end);
+    vector<int> local_primes = findPrimes(local_start, local_end);
     int local_count = local_primes.size();
 
-    std::vector<int> counts(size);
+    vector<int> counts(size);
     MPI_Gather(&local_count, 1, MPI_INT, counts.data(), 1, MPI_INT, 0, MPI_COMM_WORLD);
 
-    std::vector<int> displs(size);
+    vector<int> displs(size);
     int total = 0;
-    if (rank == 0) {
-        for (int i = 0; i < size; ++i) {
+    if (rank == 0)
+    {
+        for (int i = 0; i < size; ++i)
+        {
             displs[i] = total;
             total += counts[i];
         }
     }
 
-    std::vector<int> all_primes(total);
+    vector<int> all_primes(total);
     MPI_Gatherv(local_primes.data(), local_count, MPI_INT, all_primes.data(), counts.data(), displs.data(), MPI_INT, 0, MPI_COMM_WORLD);
 
-    if (rank == 0) {
+    if (rank == 0)
+    {
         FILE *out = fopen("out.txt", "w");
         fprintf(out, "Prime Number Search Results:\n");
         fprintf(out, "Found %d primes between %d and %d\n", total, start, end);
